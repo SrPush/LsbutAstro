198242341888cfa6b1ce45917a430be72e2fee24
SrPush <luisfepe2006@gmail.com>
2024-06-04 00:16:37 -0500

    DIos

---
+++ src/components/SliderP.jsx
@@ -0,0 +1,24 @@
+import './index.css';
+import { useState } from 'preact';
+import Slider from "react-slick";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
+import astronaut from "./assets/tengyunv1.png";
+import celebrating from "./assets/tengyunv1.png";
+import education from "./assets/tengyunv1.png";
+import taken from "./assets/tengyunv1.png";
+
+
+
+export function SliderP() {
+    return (
+        <>
+         <p>Nose como hacer un slider descente con reactD:</p>
+        </>
+    );
+}
+
+
+
+
+
